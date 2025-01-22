b0ec969416790433b34e7028341aef6ed641df83
ElsaSer <vodaelsa18@gmail.com>
2025-01-22 19:48:31 +0000

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleUpdate = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+    }));
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <button onClick={() => this.handleUpdate('good')}>Good</button>
+        <button onClick={() => this.handleUpdate('neutral')}>Neutral</button>
+        <button onClick={() => this.handleUpdate('bad')}>Bad</button>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/Feedback.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    <div>
+      <h1>Please leave feedback</h1>
+    </div>;
+  }
+}
